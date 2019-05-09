e97e547b663b56e6fb54aaff2a9a7dcc19df4e71
Durinda99 <Panorama99!!>
2019-05-09 10:54:56 +0300

    -

--- SaveCouldBase.py
+++ SaveCouldBase.py
@@ -1,2 +1,10 @@
-#111fff
-#222
+import fdb
+from fdb import services
+
+#con = fdb.connect(host='localhost', database='fit5', user='SYSDBA', password='metalink')
+con = services.connect(host='localhost', user='SYSDBA', password='metalink')
+
+bakcupname = 'd:\\Projects\\data\\11.fbk'
+con.backup('fit5', bakcupname, metadata_only=False, collect_garbage=False, )
+#con.local_backup()
+
